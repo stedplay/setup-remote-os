1cceb24ad2a4a3134b97a08f829eb9e06fdb5ce0
stedplay <stedplay@gmail.com>
2018-10-02 04:53:47 +0900

    Create connection to host of argument.
    And, check connection with `date` command.

--- ubuntu/1804/setup.py
+++ ubuntu/1804/setup.py
@@ -1,4 +1,5 @@
 import sys
+from fabric import Connection, Config
 from getpass import getpass
 
 def prepare():
@@ -8,6 +9,12 @@ def prepare():
   new_ssh_port = int(sys.argv[2])
   mail_address = sys.argv[3]
   password = getpass(f"{host.split(':')[0]}'s password? ")
+
+  # Create connection.
+  config = Config(overrides={'sudo': {'password': password, 'prompt': '[sudo] password: \n'}, 'run': {'echo': True}})
+  c = Connection(host, connect_kwargs={"password": password}, config=config)
+  # Check connection.
+  c.run('date')
 
 def setup():
   pass
