3468bc7f082100eb1b7c8105292da8492db50a8f
stedplay <stedplay@gmail.com>
2018-10-01 19:45:15 +0900

    Add arguments.
    - host (user_name@host_name:ssh_port to connect via ssh.)
    - new_ssh_port (SSH port number to use after setup)
    - mail_address (Destination to notify)

--- ubuntu/1804/setup.py
+++ ubuntu/1804/setup.py
@@ -1,5 +1,11 @@
+import sys
+
 def prepare():
-  pass
+  # Arguments.
+  print(f'sys.argv={sys.argv}')
+  host = sys.argv[1]
+  new_ssh_port = int(sys.argv[2])
+  mail_address = sys.argv[3]
 
 def setup():
   pass
